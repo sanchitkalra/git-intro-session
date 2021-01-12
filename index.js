46411b40799e70141ca78040c1a460b127a8aca2
Sanchit Kalra <sanchit.kalra1@gmail.com>
2021-01-12 21:10:33 +0530

    index.js updated

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/isGitAnyGood', (req, res) => {
+    res.send('Git is 🔥')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
